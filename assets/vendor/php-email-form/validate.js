d3fba400bd8cfb9e83d1d91ada4a59ee21df8b8b
luimstumbad00 <124089296+luimstumbad00@users.noreply.github.com>
2024-05-29 19:49:46 -0600

    Eventos

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -18,8 +18,7 @@
       let recaptcha = thisForm.getAttribute('data-recaptcha-site-key');
       
       if( ! action ) {
-        displayError(thisForm, 'The form action property is not set!');
-        return;
+        window.location.href = "home.html";
       }
       thisForm.querySelector('.loading').classList.add('d-block');
       thisForm.querySelector('.error-message').classList.remove('d-block');
@@ -76,10 +75,10 @@
     });
   }
 
-  function displayError(thisForm, error) {
+  /*function displayError(thisForm, error) {
     thisForm.querySelector('.loading').classList.remove('d-block');
     thisForm.querySelector('.error-message').innerHTML = error;
     thisForm.querySelector('.error-message').classList.add('d-block');
-  }
+  }*/
 
 })();
